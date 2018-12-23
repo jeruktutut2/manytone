a5280c8c2f277582c57fa5f451d1ab5dc4d74912
jeruktutut2 <jeruktutut2@gmail.com>
2018-12-23 15:43:41 +0800

    commit awal

---
+++ src/main/java/com/leju/manytomany/ManytomanyApplication.java
@@ -0,0 +1,14 @@
+package com.leju.manytomany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytomanyApplication.class, args);
+	}
+
+}
+
